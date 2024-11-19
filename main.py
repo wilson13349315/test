f5b9d39618f1d039ecb94ea48edcb4e0e9818aa1
zongqiangl <zongqiang.luo@rystadenergy.com>
2024-11-19 22:20:21 +0100

    new commits

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
 import streamlit as st
-from pmdarima.arima import auto_arima
 import warnings
 from Modules.connect_to_sql import MySqlConnection
-import pandas as pd
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.feature_selection import SelectKBest, f_regression
-import os
-from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-from sklearn.svm import SVR
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
-
-import Modules.send_email as send_email
 
 # Title and Description
 st.title("Clothing Price Forecast Tool")
